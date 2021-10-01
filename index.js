f1961e3e4a054328a0f127bcaf08915947158e44
Volodymyr Androsov <v.androsov@pnn.in.ua>
2021-10-01 19:50:32 +0300

    connect db + create post scheme + refactoring

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 import express from "express";
-
-const PORT = 5000;
+import mongoose from 'mongoose'
+import * as serverInfoKeys from './const/serverInfo.js';
+import Post from './models/Post.js';
 
 const app = express();
 
-app.get('/',((req, res) => {
-    res.status(200).json("Server info");
-}))
+app.use(express.json())//аби можна було читати джейсон
 
-app.listen(PORT,()=>{
-    console.log("SERVER IS LISTENING")
+app.post('/',async (req,res)=>{
+    try{
+        console.log(req.body)
+        const {author,title,content,img}=req.body;
+        const post = await Post.create({author, title, content, img});
+
+        res.status(200).json(post)
+    }catch (e){
+        res.status(500).json(e)
+    }
 })
+
+
+const startApp = async() => {
+    try{
+        await mongoose.connect(serverInfoKeys.DB_URL);
+        app.listen(serverInfoKeys.PORT,()=>{
+            console.log("SERVER IS LISTENING")
+        })
+    }catch (e){
+        console.log(e)
+    }
+}
+
+startApp();
